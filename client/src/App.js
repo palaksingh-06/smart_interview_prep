8faa2e041a991cef49b4dfba0b7fbcaf21873415
Palak Singh <palak.singh6122004@gmail.com>
2026-05-02 11:14:05 +0530

    Initial MERN setup

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:5000")
+      .then(res => setMessage(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend + Backend Connected</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
